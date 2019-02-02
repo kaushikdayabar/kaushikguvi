053476f2fb78a4561296dd6d5340e921b5312d55
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-02-02 18:32:33 +0530

    Update ex9.py

--- ex9.py
+++ ex9.py
@@ -1,10 +1,10 @@
 s=input()
 l=s.split()
-k=int(l[1])
+kau=int(l[1])
 s=input()
 l=s.split()
 l=list(map(int,l))
 sum=0
-for i in range(0,k):
+for i in range(0,kau):
     sum=sum+l[i]
 print(sum)    
