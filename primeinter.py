c42be1e900e75a0f9bf095b51b447ba69ea853b4
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-02-04 19:45:41 +0530

    Update primeinter.py

--- primeinter.py
+++ primeinter.py
@@ -1,10 +1,15 @@
 a,b=input().split()
 s,b=int(a)+1,int(b)
+l=[]
 for dau in range(s,b):
-    j,f=int(dau/2),0
-    for i in range(2,j+1):
+    j,f=int(dau/2)+1,0
+    for i in range(2,j):
     	if dau%i==0:
     		f=1
     		break
     if f==0 and dau!=1:
-    	print(" ",dau)
+    	l.append(dau)
+le=len(l)-1
+for i in range(0,le):
+	print(l[i],end=" ")
+print(l[le],end="")	
