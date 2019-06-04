d9ea009cf224374a70c26ffe810b1b4343817f43
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-04 21:30:41 +0530

    Add files via upload

---
+++ swap_even_odd.py
@@ -0,0 +1,26 @@
+kau=input()
+n=len(kau)
+
+l=[]
+for i in range(0,n):
+    l.append(kau[i])
+
+print(l)
+
+#odd length string leads to out of index and the last odd cant be swapped
+if(n%2):
+    n=n-1
+
+#swapping
+for i in range(0,n,2):
+    l[i],l[i+1]=l[i+1],l[i]  #no need of temp variable in swapping in python
+
+    
+s=""
+for i in range(0,n):
+    s=s+l[i]
+    
+print(s)   
+    
+    
+
