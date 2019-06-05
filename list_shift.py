7050020a50c86d846b0546a4984ad0b3290dadaf
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-05 21:38:17 +0530

    Update list_shift.py

--- list_shift.py
+++ list_shift.py
@@ -1,9 +1,10 @@
 n,k=list(map(int,input().split()))
 l=[]
 l=list(map(int,input().split()))
-print(l)
 
 for i in range(0,k):
     l=l[-1:-2:-1]+l[0:-1]
-
-print(l)    
+n=n-1
+for i in range(0,n):
+    print(l[i],end=" ")
+print(l[n])    
