7264eca6674db5b93248ff38fdbf6c3d02299161
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-07 18:01:00 +0530

    Add files via upload

---
+++ string_remv_vowels_rev.py
@@ -0,0 +1,9 @@
+n=int(input())
+s=input()
+l=['a','e','i','o','u','A','E','I','O','U']
+r=""
+for i in s:
+    if i not in l:
+        r+=i      
+r=r[-1:-len(r)-1:-1]#single line to reverse a string
+print(r)
