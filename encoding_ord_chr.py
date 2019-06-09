ef18e11f5e98846ca40a3dfc7a5f5b81cf0b7315
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-09 21:01:05 +0530

    Update encoding_ord_chr.py

--- encoding_ord_chr.py
+++ encoding_ord_chr.py
@@ -4,8 +4,8 @@ r=""
 
 for i in s:
     if ord(i)+3>90:
-        r+=chr(ord(i)-23)
+        r+=chr(ord(i)-23)  #ord to convert character into unicode,once 91 is reached 26 subtraction and 3 addition hence 3-26=-23 
     else:
-        r+=chr(ord(i)+3)
+        r+=chr(ord(i)+3)   #chr to convert into characeter
 
 print(r)        
