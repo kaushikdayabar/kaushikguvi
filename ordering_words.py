d47c5f1bc9d70cd96f7f321a3e2cdca96993bcfb
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-10 19:36:37 +0530

    Update ordering_words.py

--- ordering_words.py
+++ ordering_words.py
@@ -1,14 +1,18 @@
-n=int(input())-1  #to avoid out of bound error
+n=int(input())-1#to avoid out of bound error
 l=input().split()
 
 for i in range(n):
-	if len(l[i])>len(l[i+1]):  #check the length of next element,for ordering based on length
-		l[i],l[i+1]=l[i+1],l[i]  #single line swapping 
-	elif len(l[i])==len(l[i+1]):
-		sn=len(l[i])
-		for j in range(sn):
-			if ord(l[i][j])>ord(l[i+1][j]):#ord function used to sort them lexicographically
-				l[i],l[i+1]=l[i+1],l[i]
-				
+    if len(l[i])>len(l[i+1]):#check the length of next element,for ordering based on length
+        l[i],l[i+1]=l[i+1],l[i] #single line swapping 
+        
+for i in range(n):        
+    if len(l[i])==len(l[i+1]):   
+        sn=len(l[i])
+        for j in range(sn):
+            if ord(l[i][j])>ord(l[i+1][j]):#ord function used to sort them lexicographically
+                l[i],l[i+1]=l[i+1],l[i]
+                break
+                
 for i in l:
-	print(i,end=" ")
+    print(i,end=" ")
+
