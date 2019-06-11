a9b6fa9e7ea9132490da6a8c6214556befb97e12
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-11 15:19:27 +0530

    Update str_cpr_k_integers.py

--- str_cpr_k_integers.py
+++ str_cpr_k_integers.py
@@ -1,12 +1,13 @@
 s1,s2,k=input().split(' ')
 k=int(k)
-n=max(len(s1),len(s2))
+n=max(len(s1),len(s2))  #getting the range as max length out of 2 strings
 d=0
 
-for i in range(0,n):
+for i in range(n):
     if s1[i]!=s2[i]:
-        d+=1
-if d==k:
+        d+=1         #for each difference d is incremnted
+
+if d==k:           #checking whether difference = given difference
     print('yes')
 else:
     print('no')
