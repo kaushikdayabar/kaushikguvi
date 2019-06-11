e30f59a93e3b214d204ae6f1cab9ecf03ee91c81
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-11 14:01:28 +0530

    Create del_spaces.py

---
+++ del_spaces.py
@@ -0,0 +1,8 @@
+s=input()
+r=""  #empty string  required to prevent undefined error
+
+for i in s:
+    if i!=" ":  #checking for spaces
+        r=r+i #using another as strings are immutable
+                
+print(r)
