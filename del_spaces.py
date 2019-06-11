7e12f4b849f76764228c508fa6a1ad92f25728fb
kaushikdayabar <30837925+kaushikdayabar@users.noreply.github.com>
2019-06-11 13:57:08 +0530

    Delete del_spaces.py

--- del_spaces.py
+++
@@ -1,9 +0,0 @@
-#to print the retrieved string without spaces
-s=input()
-r=""  #empty string  required to prevent undefined error
-
-for i in s:
-	if i!=" ":  #checking for spaces
-		r=r+i #using another as strings are immutable
-
-print(r)
